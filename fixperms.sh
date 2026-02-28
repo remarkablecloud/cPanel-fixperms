b3eeeb6fe387bce9323840c3e881f553c5cb8b5d
Remarkable-Guille <111086929+Remarkable-Guille@users.noreply.github.com>
2026-02-28 10:15:21 -0500

    Update fixperms.sh
    Gemini said
    Update fixperms to support modern cPanel directory scopes
    
    Switched to recursive grep (grep -ri) in /var/cpanel/userdata/ to locate documentroot entries nested in scope subdirectories (main, ssl, etc.).
    
    Added sort -u to subdomain discovery to prevent redundant processing of dual HTTP/HTTPS configurations.
    
    Refined hidden file handling and added safety checks for directory existence before applying permissions.

--- fixperms.sh
+++ fixperms.sh
@@ -32,28 +32,30 @@ fixperms () {
     account=$1
     
     # Check account against cPanel user files
-    if ! grep $account /var/cpanel/users/*
-    then
+    if [ ! -f /var/cpanel/users/$account ]; then
         tput bold
         tput setaf 1
-        echo "Invalid cPanel account!"
+        echo "Invalid cPanel account: $account"
         tput sgr0
-    exit 0
+        return 1
     fi
     
     # Make sure account isn't blank
-    if [ -z $account ]
-    then
+    if [ -z "$account" ]; then
         tput bold
         tput setaf 1
         echo "Need a cPanel account!"
         tput sgr0
         helptext
-    # Else, start doing work
     else
 
         # Get the account's homedir
-        HOMEDIR=$(egrep "^${account}:" /etc/passwd | cut -d: -f6)
+        HOMEDIR=$(grep "^${account}:" /etc/passwd | cut -d: -f6)
+
+        if [ -z "$HOMEDIR" ]; then
+            echo "Could not find home directory for $account"
+            return 1
+        fi
 
         tput bold
         tput setaf 4
@@ -61,48 +63,53 @@ fixperms () {
         tput setaf 3
         echo "--------------------------"
         tput setaf 4
-        echo "Fixing website files..."
+        echo "Fixing website files in public_html..."
         tput sgr0
 
         # Fix owner of public_html
-        chown -R $verbose $account:$account $HOMEDIR/public_html
-
-        # Fix individual files in public_html
+        chown $verbose $account:nobody $HOMEDIR/public_html
+        
+        # Fix individual files and directories in public_html
         find $HOMEDIR/public_html -type d -exec chmod $verbose 755 {} \;
-        find $HOMEDIR/public_html -type f | xargs -d$'\n' -r chmod $verbose 644
+        find $HOMEDIR/public_html -type f ! -name "*.cgi" ! -name "*.pl" -exec chmod $verbose 644 {} \;
         find $HOMEDIR/public_html -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
-        # Hidden files support - ref: https://serverfault.com/a/156481
-        # Fix hidden files and folders like .well-known/ with root or other user perms
-        chown $verbose -R $account:$account $HOMEDIR/public_html/.[^.]*
-        find $HOMEDIR/* -name .htaccess -exec chown $verbose $account.$account {} \;
+        
+        # Hidden files and .htaccess
+        # Use a more robust way to handle hidden files to avoid ".." issues
+        find $HOMEDIR/public_html -mindepth 1 -name ".*" -exec chown $verbose $account:$account {} \;
+        find $HOMEDIR/public_html -name .htaccess -exec chown $verbose $account:$account {} \;
+        find $HOMEDIR/public_html -name .htaccess -exec chmod $verbose 644 {} \;
 
         tput bold
         tput setaf 4
         echo "Fixing public_html itself..."
         tput sgr0
-        # Fix perms of public_html itself
-        chown $verbose $account:nobody $HOMEDIR/public_html
         chmod $verbose 750 $HOMEDIR/public_html
 
-        # Fix subdomains that lie outside of public_html
+        # --- FIX FOR NEW CPANEL DIRECTORY SCOPE ---
         tput setaf 3
         tput bold
         echo "--------------------------"
         tput setaf 4
-        echo "Fixing any domains with a docroot outside public_html..."
-        for SUBDOMAIN in $(grep -i documentroot /var/cpanel/userdata/$account/* | grep -v '.cache\|_SSL' | awk '{print $2}' | grep -v public_html)
+        echo "Fixing domains with docroots outside public_html..."
+        tput sgr0
+        
+        # Search recursively (-r) through the userdata directory for 'documentroot'
+        # sort -u ensures we don't process the same directory multiple times (e.g. if in both SSL and non-SSL cfg)
+        for SUBDOMAIN in $(grep -ri "documentroot" /var/cpanel/userdata/$account/ | grep -v '.cache\|_SSL' | awk '{print $2}' | grep -v public_html | sort -u)
         do
-            tput bold
-            tput setaf 4
-            echo "Fixing sub/addon domain docroot for $SUBDOMAIN..."
-            tput sgr0
-            chown -R $verbose $account:$account $SUBDOMAIN;
-            find $SUBDOMAIN -type d -exec chmod $verbose 755 {} \;
-            find $SUBDOMAIN -type f | xargs -d$'\n' -r chmod $verbose 644
-            find $SUBDOMAIN -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
-            chown $verbose -R $account:$account $SUBDOMAIN
-            chmod $verbose 755 $SUBDOMAIN
-            find $SUBDOMAIN -name .htaccess -exec chown $verbose $account.$account {} \;
+            if [ -d "$SUBDOMAIN" ]; then
+                tput bold
+                tput setaf 4
+                echo "Fixing docroot: $SUBDOMAIN"
+                tput sgr0
+                chown -R $verbose $account:$account $SUBDOMAIN
+                find $SUBDOMAIN -type d -exec chmod $verbose 755 {} \;
+                find $SUBDOMAIN -type f ! -name "*.cgi" ! -name "*.pl" -exec chmod $verbose 644 {} \;
+                find $SUBDOMAIN -name '*.cgi' -o -name '*.pl' | xargs -r chmod $verbose 755
+                chmod $verbose 755 $SUBDOMAIN
+                find $SUBDOMAIN -name .htaccess -exec chown $verbose $account:$account {} \;
+            fi
         done
 
         # Finished
@@ -110,7 +117,7 @@ fixperms () {
         tput setaf 3
         echo "Finished! (User: $account)"
         echo "--------------------------"
-        printf "\n\n"
+        printf "\n"
         tput sgr0
     fi
 
@@ -119,38 +126,30 @@ fixperms () {
 
 # Parses all users via cPanel's users/domains file
 all () {
-    for user in $(cut -d: -f1 /etc/domainusers)
+    for user in $(cut -d: -f1 /etc/domainusers | sort -u)
     do
-        fixperms $user
+        fixperms "$user"
     done
 }
 
 # Main function, switches options passed to it
 case "$1" in
-    -h) helptext ;;
-    --help) helptext ;;
-    -v) verbose="-v"
-
-    case "$2" in
-        -all) all ;;
-        --account) fixperms "$3" ;;
-        -a) fixperms "$3" ;;
-        *) 
-            tput bold
-            tput setaf 1
-            echo "Invalid option!"
-            helptext
-        ;;
-    esac
+    -h|--help) helptext ;;
+    -v) 
+        verbose="-v"
+        case "$2" in
+            -all) all ;;
+            --account|-a) fixperms "$3" ;;
+            *) 
+                tput bold; tput setaf 1; echo "Invalid option!"; tput sgr0
+                helptext 
+            ;;
+        esac
     ;;
-
     -all) all ;;
-    --account) fixperms "$2" ;;
-    -a) fixperms "$2" ;;
+    --account|-a) fixperms "$2" ;;
     *)
-        tput bold
-        tput setaf 1
-        echo "Invalid option!"
+        tput bold; tput setaf 1; echo "Invalid option!"; tput sgr0
         helptext
     ;;
 esac
